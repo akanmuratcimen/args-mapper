faaf745f398b7179020cd679c0e13815da9b5720
akanmuratcimen <akanmuratcimen@gmail.com>
2019-05-23 23:13:23 +0300

    added page renderer tests for option and command type.

--- test/ArgsMapper.Tests/PageTests/PageRendererTests.cs
+++ test/ArgsMapper.Tests/PageTests/PageRendererTests.cs
@@ -29,6 +29,80 @@ namespace ArgsMapper.Tests.PageTests
     public class PageRendererTests
     {
         [Fact]
+        internal void Render_Command()
+        {
+            // Arrange
+            var renderer = new PageRenderer();
+
+            renderer.AppendCommand(
+                PageContentRowFormattingStyle.None,
+                "command-name",
+                new PageContentCommandSettings {
+                    Description = "command description."
+                }
+            );
+
+            // Act
+            var contentText = renderer.ToString();
+
+            // Assert
+            var lines = contentText.ToLines();
+
+            Assert.Single(lines);
+
+            Assert.Equal("command-name    command description.", lines[0]);
+        }
+
+        [Fact]
+        internal void Render_Command_With_NameColumnWidth()
+        {
+            // Arrange
+            var renderer = new PageRenderer();
+
+            renderer.AppendCommand(
+                PageContentRowFormattingStyle.None,
+                "command-name",
+                new PageContentCommandSettings {
+                    Description = "command description.",
+                    NameColumnWidth = 20
+                }
+            );
+
+            // Act
+            var contentText = renderer.ToString();
+
+            // Assert
+            var lines = contentText.ToLines();
+
+            Assert.Single(lines);
+
+            Assert.Equal("command-name            command description.", lines[0]);
+        }
+
+        [Fact]
+        internal void Render_Command_Without_Description()
+        {
+            // Arrange
+            var renderer = new PageRenderer();
+
+            renderer.AppendCommand(
+                PageContentRowFormattingStyle.None,
+                "command-name",
+                new PageContentCommandSettings()
+            );
+
+            // Act
+            var contentText = renderer.ToString();
+
+            // Assert
+            var lines = contentText.ToLines();
+
+            Assert.Single(lines);
+
+            Assert.Equal("command-name", lines[0]);
+        }
+
+        [Fact]
         internal void Render_Content()
         {
             // Arrange
@@ -41,6 +115,21 @@ namespace ArgsMapper.Tests.PageTests
 
             // Assert
             Assert.Equal("content line", contentText);
+        }
+
+        [Fact]
+        internal void Render_Content_Indent()
+        {
+            // Arrange
+            var renderer = new PageRenderer();
+
+            renderer.AppendText(PageContentRowFormattingStyle.Indent, "content line");
+
+            // Act
+            var contentText = renderer.ToString();
+
+            // Assert
+            Assert.Equal("  content line", contentText);
         }
 
         [Fact]
@@ -62,21 +151,6 @@ namespace ArgsMapper.Tests.PageTests
 
             Assert.Equal("content line 1", lines[0]);
             Assert.Equal("content line 2", lines[1]);
-        }
-
-        [Fact]
-        internal void Render_Content_Indent()
-        {
-            // Arrange
-            var renderer = new PageRenderer();
-
-            renderer.AppendText(PageContentRowFormattingStyle.Indent, "content line");
-
-            // Act
-            var contentText = renderer.ToString();
-
-            // Assert
-            Assert.Equal("  content line", contentText);
         }
 
         [Fact]
@@ -218,6 +292,80 @@ namespace ArgsMapper.Tests.PageTests
 
             Assert.Equal("column 1    column 2", lines[0]);
             Assert.Equal("value 1     value 2", lines[1]);
+        }
+
+        [Fact]
+        internal void Render_Option()
+        {
+            // Arrange
+            var renderer = new PageRenderer();
+
+            renderer.AppendOption(
+                PageContentRowFormattingStyle.None,
+                "option-name",
+                new PageContentOptionSettings {
+                    Description = "option description."
+                }
+            );
+
+            // Act
+            var contentText = renderer.ToString();
+
+            // Assert
+            var lines = contentText.ToLines();
+
+            Assert.Single(lines);
+
+            Assert.Equal("option-name    option description.", lines[0]);
+        }
+
+        [Fact]
+        internal void Render_Option_With_NameColumnWidth()
+        {
+            // Arrange
+            var renderer = new PageRenderer();
+
+            renderer.AppendOption(
+                PageContentRowFormattingStyle.None,
+                "option-name",
+                new PageContentOptionSettings {
+                    Description = "option description.",
+                    NameColumnWidth = 20
+                }
+            );
+
+            // Act
+            var contentText = renderer.ToString();
+
+            // Assert
+            var lines = contentText.ToLines();
+
+            Assert.Single(lines);
+
+            Assert.Equal("option-name             option description.", lines[0]);
+        }
+
+        [Fact]
+        internal void Render_Option_Without_Description()
+        {
+            // Arrange
+            var renderer = new PageRenderer();
+
+            renderer.AppendOption(
+                PageContentRowFormattingStyle.None,
+                "option-name",
+                new PageContentOptionSettings()
+            );
+
+            // Act
+            var contentText = renderer.ToString();
+
+            // Assert
+            var lines = contentText.ToLines();
+
+            Assert.Single(lines);
+
+            Assert.Equal("option-name", lines[0]);
         }
 
         [Fact]
