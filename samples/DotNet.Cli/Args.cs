ddc8004a6d11f448595ac0bdd87d2bbcebca24f6
akanmuratcimen <mcimen@boynergrup.com>
2019-05-27 09:58:50 +0300

    removed dotnet.cli sample.

--- samples/DotNet.Cli/Args.cs
+++
@@ -1,9 +0,0 @@
-namespace DotNet.Cli
-{
-    internal class Args
-    {
-        public bool Info { get; set; }
-        public bool ListSdks { get; set; }
-        public bool ListRuntimes { get; set; }
-    }
-}
